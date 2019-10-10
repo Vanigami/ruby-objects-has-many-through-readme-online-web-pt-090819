b1ab842b4f21787b377a983bba83879a6258a26d
Vanna Winland <vgwinland@gmail.com>
2019-10-10 01:38:45 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -13,7 +13,7 @@ attr_accessor :name, :yrs_experience
     @@all 
   end
 
-  def new_meal(customr, total, tip=0)
-    Meal.new(self, customr, total, tip)
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
   end
 end
